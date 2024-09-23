25b3cd9202435e0330ea7c8c6840918fe8d7d63a
Ajay Mishra <70057599+ajay-mishra7@users.noreply.github.com>
2024-09-23 18:28:48 +0530

    Update Footer.js

--- Frontend/src/components/GeneralScreens/Footer.js
+++ Frontend/src/components/GeneralScreens/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
             <div className="footer">
             </div>
             <div className="copyright">
-                <p className="copyright-blog">© 2022 Gilbert Hutapea. All Rights Reserved</p>
+                <p className="copyright-blog">© 2024 Ajay Mishra.</p>
             </div>
         </div>
     )
